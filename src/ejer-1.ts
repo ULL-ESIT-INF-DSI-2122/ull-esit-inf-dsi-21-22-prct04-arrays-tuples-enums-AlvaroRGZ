e9967551253a458d3790c3c44442d3f5ba69b890
AlvaroRGZ <alu0101362953@ull.edu.es>
2022-03-13 11:54:03 +0000

    ejer 1 doc

--- src/ejer-1.ts
+++ src/ejer-1.ts
@@ -1,3 +1,11 @@
+/**
+ * Devuelve un array de arrays con n tablas de multiplicar,
+ * donde cada tabla de multiplicar contiene los primeros n
+ * productos.
+ * @param n número de tablas de multiplicar. n >= 1
+ * @return number[][] conteniendo las tablas generadas
+ * o undefined si n < 1
+ */
 export function productTable(n: number): number[][] | undefined {
   if (n < 1) {
     return undefined;
